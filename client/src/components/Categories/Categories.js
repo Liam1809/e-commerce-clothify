6d55107adcbb6f6696c912112259857b53d04a20
Liam1809 <hkhanhlam99@gmail.com>
2022-09-27 15:20:01 +0100

    Catergories component

---
+++ client/src/components/Categories/Categories.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Category from './Category/Category';
+
+import './styles.scss';
+const arr = [
+  {
+    id: 1,
+    title: 'hats',
+    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+  },
+  {
+    id: 2,
+    title: 'jackets',
+    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+  },
+  {
+    id: 3,
+    title: 'sneakers',
+    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+  },
+  {
+    id: 4,
+    title: 'womens',
+    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+  },
+  {
+    id: 5,
+    title: 'mens',
+    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+  },
+];
+
+const Categories = ({ title }) => {
+  const CategoriesArr = arr.map((category, i) => (
+    <Category title={category.title} imageUrl={category.imageUrl} key={i} />
+  ));
+  return <div className="categories-container">{CategoriesArr}</div>;
+};
+
+export default Categories;
