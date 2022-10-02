14741b84ab4cba93ba4413404c9f375090e6a988
Liam1809 <hkhanhlam99@gmail.com>
2022-10-02 23:28:33 +0100

    implement sign-in Google and Firestore Firebase db api

---
+++ client/src/api/firebase/firebase.js
@@ -0,0 +1,69 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FB_APIKEY,
+  authDomain: process.env.REACT_APP_FB_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_FB_PROJECTID,
+  storageBucket: process.env.REACT_APP_FB_STOREAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FB_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_FB_APPID,
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+// Sign-in with Google Auth
+const googleProvider = new GoogleAuthProvider();
+
+googleProvider.setCustomParameters({
+  prompt: 'select_account',
+});
+
+// Auth Firebase
+// Initialise Auth Firesttore
+export const auth = getAuth();
+
+// sign in wih google popup
+export const signInWithGooglePopup = () =>
+  signInWithPopup(auth, googleProvider);
+
+// sign in with google direct
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, googleProvider);
+
+// Firestore Firebase
+// Initialise Firesttore database
+export const db = getFirestore();
+
+// get and create user in Firestore Firebase
+export const getAndCreateUserFireBase = async userAuth => {
+  const userDocRef = await doc(db, 'users', userAuth.uid);
+
+  const userSnapshot = await getDoc(userDocRef);
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+
+    const createdAt = new Date();
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log('error sign-in with google');
+    }
+  }
+
+  return userDocRef;
+};
--- client/src/routes/SignInRoute/SignInRoute.js
+++ client/src/routes/SignInRoute/SignInRoute.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import {
+  signInWithGooglePopup,
+  getAndCreateUserFireBase,
+} from '../../api/firebase/firebase';
 
 const SignInRoute = () => {
-  return <div>SignInRoute</div>;
+  const loginGoogle = async () => {
+    const { user } = await signInWithGooglePopup();
+    console.log(user);
+    const ok = await getAndCreateUserFireBase(user);
+
+    console.log(ok);
+  };
+  return (
+    <Fragment>
+      <h1>SignInRoute</h1>
+      <button onClick={() => loginGoogle()}>google sign in</button>
+    </Fragment>
+  );
 };
 
 export default SignInRoute;
